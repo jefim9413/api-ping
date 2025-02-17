32ed3532488f29fa1901792f33e1d26b3c8fc51e
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-17 01:11:18 -0300

    feat: adicionando rota profile

--- src/http/users/routes.ts
+++ src/http/users/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
+import { verifyJwt } from '../middlewares/verify-jwt'
+import { profile } from './profile'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
