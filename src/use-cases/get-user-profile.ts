49847c328bb448fde9759272e9abf3eee3416c9b
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-17 01:10:40 -0300

    fix: adicionando interface request e response

--- src/use-cases/get-user-profile.ts
+++ src/use-cases/get-user-profile.ts
@@ -1,10 +1,21 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { User } from '@prisma/client'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
 
 export class GetUserProfileUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute(userId: string) {
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
     const user = await this.usersRepository.findById(userId)
 
     if (!user) {
